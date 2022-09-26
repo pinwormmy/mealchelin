edb7a5ff4afe200b96f3e2329e51e6912870a95e
pinwormmy <ozkhan@naver.com>
2022-09-27 00:28:17 +0900

    인터셉터 상속 부분 스프링부트에 맞게(버전변화로 인한) 수정
    -세부메뉴에 오류 뜨는지 테스트 필요
    -실행엔 지장없는듯한데 Some input files use or override a deprecated API 메세지 확인하고 고치기

--- src/main/java/kh/project4/mealchelin/member/AdminInterceptor.java
+++ src/main/java/kh/project4/mealchelin/member/AdminInterceptor.java
@@ -4,12 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AdminInterceptor extends HandlerInterceptorAdapter{	
+public class AdminInterceptor implements HandlerInterceptor {
 
 	@Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
